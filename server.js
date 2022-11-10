c5c22a53c2d6700c6ae14149efba28c6b98d6fed
David Sheinbein <davesheinbein@gmail.com>
2022-11-10 00:19:30 -0800

    install axios

--- server.js
+++ server.js
@@ -12,98 +12,125 @@ app.use(
 	})
 );
 
-app.get('/scrape', function (req, res) {
-	url = req.query.url;
-	var $;
-	const exampleUrl = url === 'https://hypem.com/popular';
-	request(url, function (error, response, html) {
-		if (!error) {
-			$ = cheerio.load(html);
-		}
-		if (!!exampleUrl) {
-			let data = {
-				html: [$.html()],
-				rank: [],
-				thumb: [],
-				thumbnail: [],
-				artist: [],
-				baseTitle: [],
-				remixLink: [],
-				socialMedia: [],
-				socialMediaLink: [],
-				headerLogo: [
-					{
-						title: $('#header')
-							.find('.logo-txt')
-							.attr('title'),
-						href: $('#header')
-							.find('.logo-txt')
+let data = {
+	html: [],
+	rank: [],
+	thumb: [],
+	thumbnail: [],
+	artist: [],
+	baseTitle: [],
+	remixLink: [],
+	socialMedia: [],
+	socialMediaLink: [],
+	headerLogo: [],
+};
+
+let customData = {
+	html: [],
+};
+
+app.get(
+	'/',
+	(
+		// req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
+		// res: Response<ResBody, Locals>
+		req,
+		res
+	) => {
+		res.json('Hello please navigate to /scrape');
+	}
+);
+
+app.get(
+	'/scrape',
+	(
+		// req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
+		// res: Response<ResBody, Locals>
+		req,
+		res
+	) => {
+		url = req.query.url;
+		var $;
+		const exampleUrl = url === 'https://hypem.com/popular';
+		request(url, function (error, response, html) {
+			if (!error) {
+				$ = cheerio.load(html);
+			}
+			if (!!exampleUrl) {
+				data.html.push($.html());
+				data.headerLogo.push({
+					title: $('#header')
+						.find('.logo-txt')
+						.attr('title'),
+					href: $('#header').find('.logo-txt').attr('href'),
+				});
+				$('.section-player').each((idx, el) => {
+					data.rank.push({
+						value: $(el).find('.rank').text(),
+						id: idx,
+					});
+					data.thumb.push({
+						value: $(el).find('.thumb').attr('href'),
+						id: idx,
+					});
+					data.thumbnail.push({
+						value: $(el)
+							.find('.thumb')
+							.attr('style')
+							.match(/\((.*?)\)/)[1]
+							.replace(/('|")/g, ''),
+						id: idx,
+					});
+					data.artist.push({
+						value: $(el)
+							.find('.track_name > .artist')
+							.text(),
+						id: idx,
+					});
+					data.baseTitle.push({
+						value: $(el)
+							.find('.track_name > .track .base-title')
+							.text(),
+						id: idx,
+					});
+					data.remixLink.push({
+						value: $(el)
+							.find('.track_name > .track .remix-link')
+							.text(),
+						id: idx,
+					});
+					data.socialMedia.push({
+						value: $(el)
+							.find('.meta > .download > a')
+							.text(),
+						id: idx,
+					});
+					data.socialMediaLink.push({
+						value: $(el)
+							.find('.meta > .download > a')
 							.attr('href'),
-					},
-				],
-			};
+						id: idx,
+					});
+					let jsonObj = JSON.stringify(data);
 
-			$('.section-player').each((idx, el) => {
-				data.rank.push({
-					value: $(el).find('.rank').text(),
-					id: idx,
+					if (data.socialMediaLink.length >= 20) {
+						res.setHeader(
+							'Content-Type',
+							'application/json'
+						);
+						res.status(200);
+						res.json(jsonObj);
+					}
 				});
-				data.thumb.push({
-					value: $(el).find('.thumb').attr('href'),
-					id: idx,
-				});
-				data.thumbnail.push({
-					value: $(el)
-						.find('.thumb')
-						.attr('style')
-						.match(/\((.*?)\)/)[1]
-						.replace(/('|")/g, ''),
-					id: idx,
-				});
-				data.artist.push({
-					value: $(el).find('.track_name > .artist').text(),
-					id: idx,
-				});
-				data.baseTitle.push({
-					value: $(el)
-						.find('.track_name > .track .base-title')
-						.text(),
-					id: idx,
-				});
-				data.remixLink.push({
-					value: $(el)
-						.find('.track_name > .track .remix-link')
-						.text(),
-					id: idx,
-				});
-				data.socialMedia.push({
-					value: $(el).find('.meta > .download > a').text(),
-					id: idx,
-				});
-				data.socialMediaLink.push({
-					value: $(el)
-						.find('.meta > .download > a')
-						.attr('href'),
-					id: idx,
-				});
+			} else {
+				customData.push({ html: $.html() });
 				let jsonObj = JSON.stringify(data);
-
-				if (data.socialMediaLink.length >= 20) {
-					res.setHeader('Content-Type', 'application/json');
-					res.status(200);
-					res.json(jsonObj);
-				}
-			});
-		} else {
-			let data = {
-				html: $.html(),
-			};
-			let jsonObj = JSON.stringify(data);
-			res.send(jsonObj);
-			res.status(200).end();
-		}
-	});
-});
+				res.send(jsonObj);
+				res.status(200).end();
+			}
+		});
+	}
+);
 
 function haltOnTimedout(req, res, next) {
 	if (!req.timedout) next();
